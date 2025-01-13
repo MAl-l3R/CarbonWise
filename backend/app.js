cb33c4ef2eaf693d37d857fd811b5b9d59824afc
Maher <mahxr.99@gmail.com>
2025-01-12 21:59:26 -0700

    frontend + backend structure

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors())
+app.use(express.json()); // Parse JSON requests
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
